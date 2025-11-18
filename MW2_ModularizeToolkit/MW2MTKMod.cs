b66572d469c3cd23d45186b80f6ea322d3f355ef
kaitorisenkou <kaitorisenkou@icloud.com>
2025-11-18 19:09:29 +0900

    Assembly version 1.2

--- MW2_ModularizeToolkit/MW2MTKMod.cs
+++ MW2_ModularizeToolkit/MW2MTKMod.cs
@@ -11,8 +11,12 @@ namespace MW2_ModularizeToolkit {
         static Lazy<string> path_templateText = new Lazy<string>(() => Path.Combine(ContentPack.RootDir, "xml_template.txt"));
         public static string Path_templateText => path_templateText.Value;
 
+        static Lazy<string> path_templateTextForStyleDef = new Lazy<string>(() => Path.Combine(ContentPack.RootDir, "xml_template_StyleDef.txt"));
+        public static string Path_templateTextForStyleDef => path_templateTextForStyleDef.Value;
+
         static Lazy<string> path_export = new Lazy<string>(() => Path.Combine(ContentPack.RootDir, "MTK_Output", "Patches"));
         public static string Path_export(ThingDef weaponDef) => Path.Combine(path_export.Value, weaponDef.defName + ".xml");
+        public static string Path_export(ThingStyleDef tStyleDef) => Path.Combine(path_export.Value, tStyleDef.defName + ".xml");
 
         public MW2MTKMod(ModContentPack content) : base(content) {
             contentPack = content;
